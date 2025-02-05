63d4b4bb4895cb6138e24a46902bf638333914b1
Luyue28 <zhan0067@hz.nl>
2025-02-05 16:03:14 +0100

    basic structure done

---
+++ src/lib/components/NavBar.svelte
@@ -0,0 +1,64 @@
+<script>
+	import { goto } from '$app/navigation';
+</script>
+
+<nav class="fixed left-0 top-0 z-50 w-full bg-white shadow-md">
+	<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+		<div class="flex h-16 items-center justify-between">
+			<!-- App title -->
+			<div class="flex flex-1 justify-start">
+				<h1 class="text-xl font-bold text-gray-700">Password Manager</h1>
+			</div>
+
+			<!-- Create btn -->
+			<button
+				class="mr-3 flex flex-col items-center text-gray-700 hover:text-black"
+				on:click={() => goto('')}
+			>
+				<svg
+					class="h-6 w-6"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+				>
+					<path d="M19 11h-6V5h-2v6H5v2h6v6h2v-6h6z"></path>
+				</svg>
+			</button>
+			<!-- Search btn -->
+			<button
+				class="mr-3 flex flex-col items-center text-gray-700 hover:text-black"
+				on:click={() => goto('')}
+			>
+				<svg
+                    class="h-6 w-6"
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+					><path
+						d="M10 18a7.952 7.952 0 0 0 4.897-1.688l4.396 4.396 1.414-1.414-4.396-4.396A7.952 7.952 0 0 0 18 10c0-4.411-3.589-8-8-8s-8 3.589-8 8 3.589 8 8 8zm0-14c3.309 0 6 2.691 6 6s-2.691 6-6 6-6-2.691-6-6 2.691-6 6-6z"
+					></path></svg
+				>
+			</button>
+			<!-- Settings btn -->
+			<button
+				class="flex flex-col items-center text-gray-700 hover:text-black"
+				on:click={() => goto('')}
+			>
+				<svg
+					class="h-6 w-6"
+					xmlns="http://www.w3.org/2000/svg"
+					viewBox="0 0 24 24"
+					fill="currentColor"
+				>
+					<path
+						d="m2.344 15.271 2 3.46a1 1 0 0 0 1.366.365l1.396-.806c.58.457 1.221.832 1.895 1.112V21a1 1 0 0 0 1 1h4a1 1 0 0 0 1-1v-1.598a8.094 8.094 0 0 0 1.895-1.112l1.396.806c.477.275 1.091.11 1.366-.365l2-3.46a1.004 1.004 0 0 0-.365-1.366l-1.372-.793a7.683 7.683 0 0 0-.002-2.224l1.372-.793c.476-.275.641-.89.365-1.366l-2-3.46a1 1 0 0 0-1.366-.365l-1.396.806A8.034 8.034 0 0 0 15 4.598V3a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1.598A8.094 8.094 0 0 0 7.105 5.71L5.71 4.904a.999.999 0 0 0-1.366.365l-2 3.46a1.004 1.004 0 0 0 .365 1.366l1.372.793a7.683 7.683 0 0 0 0 2.224l-1.372.793c-.476.275-.641.89-.365 1.366zM12 8c2.206 0 4 1.794 4 4s-1.794 4-4 4-4-1.794-4-4 1.794-4 4-4z"
+					></path>
+				</svg>
+			</button>
+		</div>
+	</div>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import { goto } from "$app/navigation";
+    import { onMount, tick } from "svelte";
+    
+    let countdown = 2;
+    onMount(async() => {
+        while (countdown > 0) {
+			await tick();
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			countdown--;
+		}
+        goto('/main/home');
+    });
+</script>
+
+<div class="relative h-screen flex flex-col items-center">
+    <!-- Slogan -->
+    <p class="absolute top-1/3 w-full text-center italic text-gray-800">
+        "Keep your passwords safe!"
+    </p>
+
+    <!-- Title -->
+    <h1 class="absolute bottom-20 w-full text-center text-2xl font-bold flex items-center justify-center gap-2">
+        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" 
+            class="w-6 h-6 fill-current">
+            <path d="M19 4h-3V2h-2v2h-4V2H8v2H5c-1.103 0-2 .897-2 2v14c0 1.103.897 2 2 2h14c1.103 0 2-.897 2-2V6c0-1.103-.897-2-2-2zM5 20V7h14V6l.002 14H5z"></path>
+            <path d="M7 9h10v2H7zm0 4h5v2H7z"></path>
+        </svg>
+        Password Manager
+    </h1>
+</div>
